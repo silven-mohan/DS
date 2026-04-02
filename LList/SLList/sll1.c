86c95467060ff52d4f60a4af16a907aeca9583a4
silven-cslab <silvencslab@gmail.com>
2026-04-02 23:55:56 +0530

    ✨ Added: Code for all ops on sll.

--- LList/SLList/sll1.c
+++ LList/SLList/sll1.c
@@ -1,46 +1,26 @@
-/** ===== Single Linked List: Node Insertion Implementation ===== **/
+/** ===== Single Linked List: Operations Implementation ===== **/
 
 
 /** ===== Documentation ===== **/
 /*
- * This program shows the implementation of the Node Insertion operation on a Single Linked List.
+ * This program shows the implementation of the operations on a Single Linked List.
  * In this we do the following processes:
  	* Creation of the List.
 	* Initialization of the List with certain values.
-	* Display the List.
+	* Display the List by traversing.
 	* Insertion of a new node at the front or middle(any position) or at the end of the list.
-	* Again, display the new list.
- * Thus, we finally insert a new node at the required position of the list.
+	* Deletion of a node that is at front or middle(any position) or at the end of the list.
+	* Search for specific node in the list.
+	* Merge two lists into a single list and print the merged list.
+	* Reverse the single linked list.
+	* Modify a specific node's value.
+ * Thus, we finally perform all of the operations on the list.
 */
 
 
 /** ===== Preprocessing Directives ===== **/
 #include<stdio.h>	//For Basic I/O functions like printf(), scanf(),....
 #include<stdlib.h>		//For DMA functions like malloc(), free(),....
-
-
-/** ===== Function Prototypes ===== **/
-int create(struct node *);
-int init(struct node *, struct node *);
-int display(struct node *);
-int insertMain(void);
-int insertFront(void);
-int insertPos(void);
-int insertEnd(void);
-int deleteMain(void);
-int deleteFront(void);
-int deletePos(void);
-int deleteEnd(void);
-int searchMain(void);
-int search(void);
-int mergeMain(void);
-int merge(struct node*, struct node*);
-int reverseMain(void);
-int reverse(void);
-int modifyMain(void);
-int modify(void);
-
-
 
 
 /** ===== Global Declaration ===== **/
@@ -51,63 +31,133 @@ struct node {
 };
 
 struct node *header = NULL, *N = NULL, new;		//Declaraing here for global access.
-int n;
+
+
+/** ===== Function Prototypes ===== **/
+int create(struct node **, int);
+int init(struct node *, struct node *, int);
+int display(struct node *);
+int insertMain(int);
+int insertFront(void);
+int insertPos(int);
+int insertEnd(void);
+int deleteMain(void);
+int deleteFront(void);
+int deletePos(void);
+int deleteEnd(void);
+int search(void);
+int mergeMain(void);
+int merge(struct node *, struct node *);
+int reverse(void);
+int modify(int);
 
 
 /** ===== Main Function ===== **/
 int main()
 {
 	//Variable declarations:
-	int choice;
+	int choice, n;
+	char ch2;
 
 
 	printf("\n\n ====== Singly Linked List : Operations Implementations ====== \n\n");
 	printf("\n   -------------------------------------------------------------   \n");
 	printf("\n\nLet us start by creating a list and assigning each node some values...\n");
+	printf("Enter the no. of nodes in the list: ");
+	scanf("%d", &n);
 
 	printf("\nCreating the list......\n");
-	if(create(N)) return 1;	
+	if(create(&header, 1))  return 1;
+	if(create(&N, n)) return 1;	
+	printf("\nSuccessfully created the list.");
 	 	
 	printf("\nInitializing the list.....\n");
-	init(header, N);
+	init(header, N, n);
 
-	printf("\n===============================\n\n");
-	printf("\nChoose the operations that are to be performed on the list: \n");
-	printf("\n1. Traversal\n2. Insertion of Nodes\n3. Deletion of Nodes\n4. Searching of a node\n5. Merging of two lists\n6. Reversing of a list\n7. Modify a node's value\n8. Exit\n");
-	scanf("%d", &choice);
-	printf("\n===============================\n\n");
-
-	//Deciding the user's choice:
-	switch(choice)
+	do
 	{
-		case 1:
-			{
-				printf("Traversing...\n");
-				if(display(header))  return 1;
-				break;
-			}
-		case 2:
-			{
-				printf("Insertion...\n");
-				if(insertMain())  return 1;
-				break;
-			}
-		case 3:
-			{
-				printf("Deletion...\n");
-				if(deleteMain())  return 1;
-				break;
-			}
-		case 4:
-			{
-				printf("Searching...\n");
-				if(searchMain())  return 1;
-				break;
-			}
-	}
+		printf("\n===============================\n\n");
+		printf("\nChoose the operations that are to be performed on the list: \n");
+		printf("\n1. Traversal\n2. Insertion of Nodes\n3. Deletion of Nodes\n4. Searching of a node\n5. Merging of two lists\n6. Reversing of a list\n7. Modify a node's value\n8. Exit\n");
+		scanf("%d", &choice);
+		printf("\n===============================\n\n");
 
+		//Deciding the user's choice:
+		switch(choice)
+		{
+			case 1:
+				{
+					printf("Traversing...\n");
+					if(display(header))  return 1;
+					break;
+				}
+			case 2:
+				{
+					printf("Insertion...\n");
+					if(insertMain(n))  return 1;
+					break;
+				}
+			case 3:
+				{
+					printf("Deletion...\n");
+					if(deleteMain())  return 1;
+					break;
+				}
+			case 4:
+				{
+					printf("Searching...\n");
+					printf("The list is: \n");
+					if(display(header))  return 1;
+					
+					if(search())  return 1;
+					break;
+				}
+			case 5:
+				{
+					printf("Merging two lists....\n");
+					if(mergeMain())  return 1;
+					break;
+				}
+			case 6:
+				{
+					printf("Reversing the list....\n");
+					printf("The list is: \n");
+					if(display(header))  return 1;
+					
+					if(reverse())  return 1;
+					break;
+				}
+			case 7:
+				{
+					printf("Modifying a node's value...\n");
+					printf("The list is: \n");
+					if(display(header))  return 1;
+					
+					if(modify(n))  return 1;
+					break;
+				}
+			case 8:
+				{
+					printf("Exit...\n");
+					goto l;
+					break;
+				}
+			default:
+				{
+					printf("Invalid option. Try Again!!\n");
+				}
+		}
 
-
+		printf("\nDo you want to continue?(Y/n)\n");
+		scanf(" %c", &ch2);
+		
+	}while(ch2 == 'Y');
+	l:
+	
+	//free the allocated memory:
+	free(header);  header = NULL;
+	free(N);  N = NULL;
+	
 	return 0;
 }
 
@@ -116,28 +166,22 @@ int main()
 //create():
 //This function allocates the memory for the nodes.
 
-int create(struct node *N)
+int create(struct node **N, int size)
 {
-	//Taking the total no. of nodes from the user:
-	printf("\nEnter the no. of nodes of the list: ");
-	scanf("%d", &n);
-
-	if(n < 1)
+	if(size < 1)
 	{
 		printf("\nInsufficient nodes to create a list!!\n\n");		//Handling edge cases.
 		return 1;
 	}
 
 	//Allocating the memory for the n nodes.
-	*N = malloc(n * sizeof(struct node));
+	*N = malloc(size * sizeof(struct node));
 
 	if(*N == NULL)
 	{
 		printf("\nMemory allocation failed!");		//Checking if memory allocation is done or not.
 		return 1;				
 	}
-
-	printf("\nSuccessfully created the list.\n");
 
 	return 0;
 }
@@ -146,7 +190,7 @@ int create(struct node *N)
 //init():
 //This function initializes all of the nodes in the list with a certain values.
 
-int init(struct node* header, struct node* N)
+int init(struct node* header, struct node* N, int size)
 {
 	int i = 0;
 
@@ -155,10 +199,10 @@ int init(struct node* header, struct node* N)
 
 	//Taking the data of all the nodes.
 	printf("\nEnter the data of all of the nodes: ");
-	while(i < n)
+	while(i < size)
 	{
 		scanf("%d", &((N+i) -> data));
-		if(i == n-1)
+		if(i == size-1)
 		{
 			(N+i) -> next = NULL;
 			return 0;
@@ -205,7 +249,7 @@ int display(struct node *header)
 //insertMain():
 //This function created the new node that is to be inserted and determines where it should be inserted(front, end, or any other position)
 
-int insertMain()
+int insertMain(int size)
 {
 	int choice1;
 
@@ -220,6 +264,10 @@ int insertMain()
 	printf("\n1. Front\n2. At any position 'n'\n3. End\n");
 	printf("\nChoose one of the option: ");
 	scanf(" %d", &choice1);
+	
+	//Printing the list before insertion:
+	printf("\nThe list before insertion: ");
+	if(display(header)) return 1;
 
 	//Execution based on the choice:
 	if(choice1 == 1)
@@ -228,7 +276,7 @@ int insertMain()
 	}
 	else if(choice1 == 2)
 	{
-		if(insertPos()) return 1;
+		if(insertPos(size)) return 1;
 	}
 	else if(choice1 == 3)
 	{
@@ -240,7 +288,7 @@ int insertMain()
 		return 1;
 	}
 	printf("\nSuccessfully inserted the node.\nThe list after the node insertion is: ");
-	display();
+	if(display(header))  return 1;
 
 	return 0;
 }
@@ -253,10 +301,10 @@ int insertFront()
 {
 	struct node *temp;
 
-	temp = header.next;
+	temp = header -> next;
 
 	//Linking the new node to the header node.
-	header.next = &new;
+	header -> next = &new;
 
 	//Linking the list to the new node that is inserted at the front position.
 	new.next = temp;
@@ -268,13 +316,13 @@ int insertFront()
 //insertPos():
 //This function inserts a node at the specified position.
 
-int insertPos()
+int insertPos(int size)
 {
-	int pos, j=0;
+	int pos, j = 0;
 	printf("\nAt which position do you want to insert the node: ");
 	scanf(" %d", &pos);
 
-	if(pos < 0 || pos > n+1)
+	if(pos < 0 || pos > size+1)
 	{
 		printf("\nCannot insert at that position.\n");
 		return 1;
@@ -285,7 +333,7 @@ int insertPos()
 		insertFront();
 		return 0;
 	}
-	if(pos == n+1)
+	if(pos == size+1)
 	{
 		insertEnd();
 		return 0;
@@ -293,7 +341,7 @@ int insertPos()
 	
 	struct node *temp = NULL, *prev = NULL;
 
-	temp = header.next;
+	temp = header -> next;
 
 	while(temp != NULL && j < pos)
 	{
@@ -308,9 +356,6 @@ int insertPos()
 	//Linking the nodes from the position 'pos' to the the new node:
 	new.next = temp;
 
-	//Display the modified list:
-	display();
-
 	return 0;
 }
 
@@ -322,7 +367,7 @@ int insertEnd()
 {
 	struct node *temp;
 
-	temp = header.next;
+	temp = header -> next;
 
 
 	//Traversing till the last node:
@@ -351,6 +396,10 @@ int deleteMain()
 	printf("\n1. Node at Front\n2. Node at any Position\n3. Node at End\n");
 	printf("\nChoose one of the option: ");
 	scanf("%d", &choice1);
+	
+	//Printing the original list:
+	printf("\nThe list before deletion: \n");
+	if(display(header))  return 1;
 
 	if(choice1 == 1)
 	{
@@ -371,7 +420,7 @@ int deleteMain()
 	}
 
 	printf("\nSuccessfully deleted the node.\n");
-	if(display()) return 1;				//Displaying the new list.
+	if(display(header)) return 1;				//Displaying the new list.
 
 	return 0;
 }
@@ -383,7 +432,7 @@ int deleteMain()
 int deleteFront()
 {
 	//Checking if the list is empty:
-	if(header.next == NULL)
+	if(header -> next == NULL)
 	{
 		printf("\nThe list is empty!!");
 		return 1;
@@ -391,16 +440,16 @@ int deleteFront()
 
 	struct node *temp = NULL;
 
-	temp = header.next;		//Now temp points to the front node.
+	temp = header -> next;		//Now temp points to the front node.
 	
 	//If the list contains only one node:
 	if(temp -> next == NULL)
 	{
-		header.next == NULL;	
+		header -> next == NULL;	
 		return 0;
 	}
 
-	header.next = temp -> next;		//Now temp -> next points to the second node.
+	header -> next = temp -> next;		//Now temp -> next points to the second node.
 
 	return 0;
 }
@@ -414,7 +463,7 @@ int deletePos()
 	int pos, j = 0;
 
 	//Checking if the list is empty:
-	if(header.next == NULL)
+	if(header -> next == NULL)
 	{
 		printf("\nThe list is empty!!\n");
 		return 1;
@@ -425,7 +474,7 @@ int deletePos()
 	scanf("%d", &pos);
 
 	struct node *temp = NULL, *prev = NULL;
-	temp = header.next;
+	temp = header -> next;
 
 	while(temp != NULL && j < pos)
 	{
@@ -447,19 +496,19 @@ int deletePos()
 int deleteEnd()
 {
 	//Checking if the list is empty:
-	if(header.next == NULL)
+	if(header -> next == NULL)
 	{
 		printf("\nThe list is empty!!\n");
 		return 1;
 	}
 
 	struct node *temp = NULL, *prev = NULL;
-	temp = header.next;
+	temp = header -> next;
 
 	//If the list has only one node:
 	if(temp -> next == NULL)
 	{
-		header.next = NULL;
+		header -> next = NULL;
 		return 0;	
 	}
 
@@ -489,7 +538,7 @@ int search()
 	scanf("%d", &key);
 
 	struct node *temp = NULL;
-	temp = &header;
+	temp = header;
 
 	//Traversing through the entire list:
 	while(temp -> next != NULL)
@@ -509,17 +558,57 @@ int search()
 	return 1;
 }
 
+//mergeMain():
+//This function acts as the main function for merge operation.
 
+int mergeMain(void)
+{
+	struct node *N2 = NULL, *header2 = NULL;
+	int n2;
+	
+	//Allocating the memory:
+	if(create(&header2, 1))  return 1;
+	
+	//Getting the second list:
+	printf("\nEnter the no. of nodes in the second list: ");
+	scanf("%d", &n2);
+	
+	//Allocating memory for all of the nodes in second list:
+	if(create(&N2, n2)) return 1;
+	init(header2, N2, n2);
+	
+	//Printing the first list:
+	printf("\nThe first list is: \n");
+	if(display(header))  return 1;
+	
+	//Printing the second list:
+	printf("\nThe second list is: \n");
+	if(display(header2))  return 1;
+	
+	//Now merging the two lists into a single one:
+	merge(header, header2);
+	
+	//free the allocated memory:
+	free(header2);  header2 = NULL;
+	free(N2);  N2 = NULL;
+	
+	return 0;
+}
 //merge():
 //This function merges two linked lists as a single list.
 
 int merge(struct node *h1, struct node *h2)
 {
+	struct node *header3 = NULL;
+
+	//Allocating memory:
+	if(create(&header3, 1))  return 1;
+
 	//Linking the first list to the header:
-	header3->next = h1->next;
+	header3 -> next = h1 -> next;
 
 	struct node *temp = NULL;
-	temp = header3->next;
+	temp = header3 -> next;
 
 	//Traversing till the last node of the first list:
 	while(temp -> next != NULL)
@@ -528,7 +617,14 @@ int merge(struct node *h1, struct node *h2)
 	}
 
 	//Linking last node of list1 to first node of list2:
-	temp -> next = h2->next;
+	temp -> next = h2 -> next;
+	
+	//Printing the merged list:
+	printf("\nThe merged list is: \n");
+	if(display(header3))  return 1;
+
+	//free the allocated memory:
+	free(header3);  header3 = NULL;
 
 	return 0;
 }
@@ -542,7 +638,7 @@ int reverse()
 	struct node *prev = NULL, *curr = NULL, *next = NULL;
 
 	//Store the first node in curr:
-	curr = header.next;
+	curr = header -> next;
 
 	while(curr != NULL)
 	{
@@ -553,10 +649,10 @@ int reverse()
 	}
 
 	//Linking the last node to the header node:
-	header.next = prev;
+	header -> next = prev;
 
 	printf("\nThe reversed list is: ");
-	if(display()) return 1;
+	if(display(header)) return 1;
 
 	return 0;
 }
@@ -565,7 +661,7 @@ int reverse()
 //modify():
 //This function takes the position of a node in list and modifies its value.
 
-int modify()
+int modify(int size)
 {
 	int key, i = 0, newValue;
 	
@@ -578,14 +674,14 @@ int modify()
 	scanf("%d", &newValue);
 
 	//Checking if position is out of bounds:
-	if(key < 1 || key >= n)
+	if(key < 1 || key >= size)
 	{	
 		printf("\nPosition of the node is out of bounds. Can't Access!!\n");
 		return 1;
 	}
 
 	struct node *temp = NULL;
-	temp = header.next;
+	temp = header -> next;
 
 	//Traversing through the entire list:
 	while(temp != NULL)
@@ -601,7 +697,7 @@ int modify()
 	}	
 
 	printf("\nThe modified list is: \n");
-	if(display()) return 1;		//Display the modified list.
+	if(display(header)) return 1;		//Display the modified list.
 	
 	return 0;
 }
